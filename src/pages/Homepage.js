3e58d68a522eb5824c61e70b3179378b2dae4a89
praneeth <gp.gunturu@gmail.com>
2025-04-07 18:57:15 -0500

    Added Link to /products imported from react-router-dom in Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom"
+
 function Homepage() {
-    return <h1>Home page</h1>
+    return (
+        <>
+            <h1>Home page</h1>
+            <p>Go to <Link to="/products">products</Link></p>
+        </>
+    )
 }
 
 export default Homepage
