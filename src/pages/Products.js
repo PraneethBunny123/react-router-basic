87650b0d2c54049e6a8d4096e7357402d870687d
praneeth <gp.gunturu@gmail.com>
2025-04-07 20:51:38 -0500

    Added a dummy PRODUCTS array and mapping them to display the products as links in the Products page. The link is given to product.id. when the product linnk is clicked the ProductDetails page will open and the link path will be rendered

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom"
+
+const PRODUCTS = [
+    {id: 'p1', title: 'product 1'},
+    {id: 'p2', title: 'product 2'},
+    {id: 'p3', title: 'product 3'}
+]
+
 export default function Products() {
     return (
         <>
             <h1>Products Page</h1>
             <ul>
-                <li>P1</li>
-                <l1>p2</l1>        
+                {PRODUCTS.map(prod => (
+                    <li key={prod.id}>
+                        <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+                    </li>
+                ))}      
             </ul>
 
         </>
